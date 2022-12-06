039e8a95fa77f1cf2173e82aa6a184675bf0c2bc
Damian Hanusiak <hanusiak.damian@gmail.com>
2022-12-06 18:20:37 +0100

    add menu and shadow header funcionality

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const menu = document.querySelector("nav");
 const burgerBtn = document.querySelector(".hamburger");
+const header = document.querySelector("header");
 
-burgerBtn.addEventListener("click", () => {
+const shadowHeader = () => {
+	const scroll = window.scrollY;
+
+	scroll > 0
+		? header.classList.add("active")
+		: header.classList.remove("active");
+};
+
+const showMenu = () => {
 	menu.classList.toggle("active");
 	burgerBtn.classList.toggle("is-active");
-});
+};
+
+burgerBtn.addEventListener("click", showMenu);
+window.addEventListener("scroll", shadowHeader);
