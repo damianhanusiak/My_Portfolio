445912162c85ff1327a6da99dd2e610fcadd92dc
Damian Hanusiak <hanusiak.damian@gmail.com>
2022-12-13 18:38:57 +0100

    create menu functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector("nav");
 const burgerBtn = document.querySelector(".hamburger");
 const header = document.querySelector("header");
+const navItems = document.querySelectorAll(".nav-link");
 
 const shadowHeader = () => {
 	const scroll = window.scrollY;
@@ -15,5 +16,20 @@ const showMenu = () => {
 	burgerBtn.classList.toggle("is-active");
 };
 
+const switchToSection = e => {
+	const key = e.target.dataset.key;
+
+	const section =
+		document.querySelector(`.${key}`).getBoundingClientRect().top +
+		window.pageYOffset -
+		135;
+
+	menu.classList.remove("active");
+	burgerBtn.classList.remove("is-active");
+
+	window.scrollTo({ top: section, behavior: "smooth" });
+};
+
 burgerBtn.addEventListener("click", showMenu);
 window.addEventListener("scroll", shadowHeader);
+navItems.forEach(item => item.addEventListener("click", switchToSection));
